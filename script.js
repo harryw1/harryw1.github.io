df07d6a939aa54c1eced829e569066ed38c12832
Harry Weiss <harrisonrweiss1@gmail.com>
2024-09-11 12:00:10 -0400

    removed unused js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("Script loaded");
-
   // Back to top button
   const backToTopButton = document.querySelector(".back-to-top");
   window.addEventListener("scroll", () => {
@@ -12,39 +10,14 @@ document.addEventListener("DOMContentLoaded", function () {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
 
-  // Lazy load images and other media
-  const lazyElements = Array.from(
-    document.querySelectorAll("img.lazy, iframe.lazy"),
-  );
-  if ("IntersectionObserver" in window) {
-    const lazyObserver = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          const lazyElement = entry.target;
-          lazyElement.src = lazyElement.dataset.src;
-          lazyElement.classList.remove("lazy");
-          lazyObserver.unobserve(lazyElement);
-        }
-      });
-    });
-
-    lazyElements.forEach((lazyElement) => {
-      lazyObserver.observe(lazyElement);
-    });
-  }
-
   // Dark mode toggle
   const darkModeToggle = document.querySelector(".dark-mode-toggle");
   const body = document.body;
-
-  // Check for saved dark mode preference
   if (localStorage.getItem("darkMode") === "true") {
     body.classList.add("dark-mode");
   }
-
   darkModeToggle.addEventListener("click", function () {
     body.classList.toggle("dark-mode");
-    // Save user preference
     localStorage.setItem("darkMode", body.classList.contains("dark-mode"));
   });
 });
